789dcf30a2afc55c58f43e12acc00dac3ed9c93c
Ryan Fernandes <ryan@fernandes.us>
2024-09-16 16:32:06 -0400

    SoC from task1a with print and append functions

--- Task1b.js
+++ Task1b.js
@@ -17,12 +17,29 @@ todoList = [];
  * the "// YOUR CODE HERE" comments.
  */
 
-function __todo(todoString) {
-    // YOUR CODE HERE
+//My function from task1a
+//Function to format and return a date string in the format "Month #/Day"
+function dateString(today) {
+    //I figured out how to use these formatted strings by looking on google because I know how to do them in Python and assumed there was something similar for js
+    return `${today.getMonth() + 1}/${today.getDate()}`;
 }
 
-function __todo() {
-    // YOUR CODE HERE
+//Appends the specified todoString to the list array
+function addTodo(todoString) {
+    //Adds todoString to the list
+    todoList.push(todoString);
+}
+
+//Prints the todoList in a formatted way with a header and numbered items
+function printTodo() {
+    //Prints header (I found info about the Java Date object here (I thought it would be cool): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date)
+    console.log(`\n-- To Do ${dateString(new Date())} --\n`);
+
+    //Iterates through each task and prints it with the number before it
+    for(let i = 0; i < todoList.length; i++) {
+        console.log(`${i+1}. ${todoList[i]}`);
+    }
+    console.log();
 }
 
 /**
@@ -31,7 +48,13 @@ function __todo() {
  * we do it here
  */
 function mainTask1b() {
-    // YOUR CODE HERE (simply call the functions you implemented above)
+    //Adding the tasks (same as before)
+    addTodo("Apply to y/cs!");
+    addTodo("Pick out my classes");
+    addTodo("Have dinner with my suite");
+
+    //Printing the to-do list
+    printTodo();
 }
 
 // This bit of code ensures that a main method exists! If it doesn't, then it throws an error
